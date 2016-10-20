f8605c2c78aca49c2123eb5694c0f7a407f4f43c
Honza Pokorny <me@honza.ca>
2016-10-20 15:24:55 -0300

    Add css

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 BUILD=_build
 python main.py -c -d $BUILD
 pandoc $BUILD/1689.md -o $BUILD/1689.epub
+pandoc -s --css=style.css $BUILD/1689.md -o $BUILD/1689.html
 ebook-convert $BUILD/1689.epub $BUILD/1689.mobi
 xelatex $BUILD/1689.tex
 rm 1689.aux 1689.log
 mv 1689.pdf $BUILD/
+cp style.css $BUILD/
