6bede85cc8fd9f1609a25fd774e10c09f0a9addf
Honza Pokorny <me@honza.ca>
2017-01-26 14:00:55 -0400

    Add some echos to compile

--- compile.sh
+++ compile.sh
@@ -1,8 +1,20 @@
+set -e
+
 BUILD=_build
-python main.py -c -e -d $BUILD
+
+echo 'Compiling...'
+python main.py -c -d $BUILD
+
+echo 'Building epub...'
 pandoc $BUILD/1689.md -o $BUILD/1689.epub
+
+echo 'Building HTML...'
 pandoc -s --css=style.css $BUILD/1689.md -o $BUILD/1689.html
+
+echo 'Building mobi...'
 ebook-convert $BUILD/1689.epub $BUILD/1689.mobi
+
+echo 'Building PDF...'
 xelatex $BUILD/1689.tex
 rm 1689.aux 1689.log
 mv 1689.pdf $BUILD/
